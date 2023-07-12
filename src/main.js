b95b953749ea0b6db52bbcb9fe5aff9d057a22dd
huanghan <1553014513@qq.com>
2023-07-12 22:45:28 +0800

    项目构建

---
+++ src/main.js
@@ -0,0 +1,20 @@
+import './assets/base.scss'
+
+import { createApp } from 'vue'
+
+import App from './App.vue'
+import router from './router'
+
+import VueCookies from 'vue-cookies'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import '@/assets/icon/iconfont.css'
+
+const app = createApp(App)
+
+app.use(router)
+app.use(ElementPlus)
+
+app.config.globalProperties.VueCookies = VueCookies
+
+app.mount('#app')
